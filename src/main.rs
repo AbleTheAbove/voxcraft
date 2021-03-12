a8bfff2c7dc5711f712504e6fd522035053cd6fd
able <abl3theabove@gmail.com>
2021-03-12 06:17:31 -0600

    commit

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let mut config = config::load_config();
     // dont leave this in
     config.height -= 1;
-    println!("{:?}", config);
+    println!("{:?}", config); // For testing
     addons::load_addons();
     if startup::addon_folder_exists() {
         println!("Good to go!");
