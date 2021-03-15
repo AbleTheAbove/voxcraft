27b4165b804ee6ec42597b8f72b8bbcfe10d44b2
able <abl3theabove@gmail.com>
2021-03-15 00:24:17 -0500

    Documentation, notes and debug tools added!

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ mod startup;
 
 pub const ROOT_PATH: &str = ".rustcraft";
 pub const GAME_NAME: &str = env!("CARGO_PKG_NAME");
+pub const GAME_VERSION: &str = env!("CARGO_PKG_VERSION");
 fn main() {
     let root = startup::get_root();
-
+    let game_meta_bar = format!("{} ver{}", GAME_NAME, GAME_VERSION);
     let mut config = config::load_config();
     // dont leave this in
     config.height -= 1 + -1; // weird math to make the compiler not throw a mutable warning
@@ -19,40 +20,50 @@ fn main() {
 
     let text;
     if startup::addon_folder_exists() {
-        text = "Good to go!";
+        text = "Addon folder found!"; // TODO(*): Sensible names for these variables
     } else {
         text = "Rustcraft folder can't be found.";
     }
 
     let mut window: PistonWindow = WindowSettings::new(
-        GAME_NAME,
+        game_meta_bar,
         [config.width as u32, config.height as u32], // u32 is required here
     )
-    .exit_on_esc(true)
+    .exit_on_esc(true) // NOTE(Able): remove in production! This is just used for quick exits
     //        .opengl(OpenGL::V2_1) // Set a different OpenGl version
     .build()
     .unwrap();
 
     let assets = root.join("assets");
     let mut glyphs = window
+        // TODO(Able): Provide a texture pack loading option with a fall back to a built in texture pack
         .load_font(assets.join("FiraSans-Regular.ttf"))
-        .unwrap();
-
-    window.set_lazy(true);
+        .unwrap(); // TODO(Able): Handle errors here and remove the unwrap
+    let ui_color = [1.0, 1.0, 1.0, 1.0];
+    // IDEA(Able): Provide this as an option > More testing needed
+    window.set_lazy(true); // NOTE(Able): Only updates when it needs too
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, device| {
-            clear([0.0, 0.0, 0.0, 1.0], g);
+            clear([0.0, 0.0, 0.0, 1.0], g); // NOTE(Able): Clears to black with 100%
 
             let transform = c
                 .transform
-                .trans(0.0 + config.ui_scale as f64, 12.0 * config.ui_scale as f64); // TODO(Able): Tweak the ui scaling placement
+                .trans(0.0 + config.ui_scale as f64, 12.0 * config.ui_scale as f64); // TODO(Able): Tweak the ui scaling
 
-            text::Text::new_color([0.0, 1.0, 0.0, 1.0], font_size as u32)
+            text::Text::new_color(ui_color, font_size as u32)
                 .draw(text, &mut glyphs, &c.draw_state, transform, g)
+                .unwrap();
+
+            let transform2 = c
+                .transform
+                .trans(0.0 + config.ui_scale as f64, 24.0 * config.ui_scale as f64); // TODO(Able): Tweak the ui scaling
+            let version_string = format!("Game version: {}", GAME_VERSION);
+            text::Text::new_color(ui_color, font_size as u32)
+                .draw(&version_string, &mut glyphs, &c.draw_state, transform2, g)
                 .unwrap();
 
             // Update glyphs before rendering.
             glyphs.factory.encoder.flush(device);
-        });
+        }); // NOTE(Able): Draw the UI last
     }
 }
