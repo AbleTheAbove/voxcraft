213ad3cc3e479fbd2d82cafaaa1409e590f6bd40
able <abl3theabove@gmail.com>
2021-03-21 20:34:38 -0500

    More architecture set up

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,6 @@ fn main() {
         [config.width as u32, config.height as u32], // u32 is required here
     )
     .exit_on_esc(true) // NOTE(Able): remove in production! This is just used for quick exits
-    //        .opengl(OpenGL::V2_1) // Set a different OpenGl version
     .build()
     .unwrap();
 
@@ -42,36 +41,44 @@ fn main() {
     // IDEA(Able): Provide this as an option > More testing needed
     window.set_lazy(true); // NOTE(Able): Only updates when it needs too
     while let Some(e) = window.next() {
-        window.draw_3d(&e, |window| {
-            let args = e.render_args().unwrap(); // NOTE(Able):  arguments such as window size
+        window.draw_3d(&e, |_window| {
+            let _args = e.render_args().unwrap(); // NOTE(Able):  arguments such as window size
 
             // TODO(Able): sort out how to do this
             // println!("{:?}", window);
-        });
+        }); // Draw the 3D world first
+        window.draw_3d(&e, |_window| {
+            let _args = e.render_args().unwrap(); // NOTE(Able):  arguments such as window size
+        }); // draw the particles here
         window.draw_2d(&e, |c, g, device| {
-            clear([0.0, 0.0, 0.0, 0.0], g); // NOTE(Able): Clears to black with 100%
+            // NOTE(Able): Clears to black with 100%
+            clear([0.0, 0.0, 0.0, 0.0], g);
+            // Startup UI
+            {
+                let startup_transform = c
+                    .transform
+                    .trans(1.0 * config.ui_scale as f64, 12.0 * config.ui_scale as f64); // TODO(Able): Tweak the ui scaling
 
-            let startup_transform = c
-                .transform
-                .trans(1.0 * config.ui_scale as f64, 12.0 * config.ui_scale as f64); // TODO(Able): Tweak the ui scaling
-
-            text::Text::new_color(ui_color, font_size as u32)
-                .draw(text, &mut glyphs, &c.draw_state, startup_transform, g)
-                .unwrap();
-
-            let version_transform = c
-                .transform
-                .trans(1.0 * config.ui_scale as f64, 24.0 * config.ui_scale as f64); // TODO(Able): Tweak the ui scaling
-            let version_string = format!("Game version: {}", GAME_VERSION);
-            text::Text::new_color(ui_color, font_size as u32)
-                .draw(
-                    &version_string,
-                    &mut glyphs,
-                    &c.draw_state,
-                    version_transform,
-                    g,
-                )
-                .unwrap();
+                text::Text::new_color(ui_color, font_size as u32)
+                    .draw(text, &mut glyphs, &c.draw_state, startup_transform, g)
+                    .unwrap();
+            }
+            // Version UI
+            {
+                let version_transform = c
+                    .transform
+                    .trans(1.0 * config.ui_scale as f64, 24.0 * config.ui_scale as f64); // TODO(Able): Tweak the ui scaling
+                let version_string = format!("Game version: {}", GAME_VERSION);
+                text::Text::new_color(ui_color, font_size as u32)
+                    .draw(
+                        &version_string,
+                        &mut glyphs,
+                        &c.draw_state,
+                        version_transform,
+                        g,
+                    )
+                    .unwrap();
+            }
 
             // Update glyphs before rendering.
             glyphs.factory.encoder.flush(device);
